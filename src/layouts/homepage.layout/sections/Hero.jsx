522cdcce9f7ff41ea5246782d30f3ec0f9320cd9
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-12-10 10:13:21 -0600

    Layout hero section

--- src/layouts/homepage.layout/sections/Hero.jsx
+++ src/layouts/homepage.layout/sections/Hero.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
+import { 
+  FaInstagram,
+  FaLinkedin,
+  FaSoundcloud, 
+  FaTwitter,
+} from 'react-icons/fa';
 
 import {
   Box,
+  Flex,
   Heading,
+  Text,
 } from '@chakra-ui/react';
 
+//TODO: Add Highlights to hero text
+// Social Links: Twitter, LinkedIn, Instagram, SoundCloud
 const Hero = () => {
+  const iconSize = '58px';
   return (
-    <Box>
-      <Heading>Hero Here</Heading>
+    <Box as='section' p='64px' bg='black'>
+      <Flex direction="column">
+        <Box w='80%'>
+          <Heading as='h1' fontSize='64px'>
+            <Text noOfLines={3} color='whiteAlpha.900'>
+              I'm Jim. A music producer, passionate about helping aspiring rappers discover their sound.
+            </Text>
+          </Heading>
+        </Box>
+        <Flex mt='32px' w='30%' justify='space-between' color='whiteAlpha.600'>
+          <FaSoundcloud fontSize={iconSize}/>
+          <FaTwitter fontSize={iconSize}/>
+          <FaInstagram fontSize={iconSize}/>
+          <FaLinkedin fontSize={iconSize}/>
+        </Flex>
+      </Flex>
     </Box>
   );
 }
