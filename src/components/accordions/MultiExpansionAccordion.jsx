1559e4b8c04fd9c5b2cfda8bf2a1241807439b90
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-12-20 16:31:41 -0600

    Refactor FAQs
    Replace hardcode with mapping over accordion items array.

--- src/components/accordions/MultiExpansionAccordion.jsx
+++ src/components/accordions/MultiExpansionAccordion.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {accordionItems as data} from '../../data/accordion-items.data';
 import {
   Accordion,
   AccordionItem,
@@ -14,98 +15,41 @@ const MultiExpansionAccordion = () => {
   return (
     <Box bg='white' py='16px' rounded='xl'>
       <Accordion allowMultiple>
-        <AccordionItem>
-          <Heading as='h3'>
-            <AccordionButton  color='blackAlpha.900'
-                              _expanded={{bg: 'yellow.400', color: 'white' }} 
-                              fontSize='32px'>
-              <Box flex='1' textAlign='left' fontSize='24px'>
-                <Text fontSize='24px' noOfLines={2} color='blackAlpha.900' py='8px' fontWeight='thin'>
-                  How long is the average session?
-                </Text>
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </Heading>
-          <AccordionPanel pb={4}>
-            <Text fontSize='20px' color='blackAlpha.700'>
-              Sessions range from 1 hour to 3 hours. For most clients, the typical session
-              is roughly 2 hours. If more time is needed, arrangements can be made 2 days before
-              the scheduled session. Contact me to discuss more details.
-            </Text>
-          </AccordionPanel>
-        </AccordionItem>
-
-        <AccordionItem>
-          <Heading as='h3'>
-            <AccordionButton  color='blackAlpha.900' 
-                              _expanded={{ bg: 'yellow.400', color: 'white' }} 
-                              fontSize='32px'>
-              <Box flex='1' textAlign='left'>
-                <Text fontSize='24px' noOfLines={2} color='blackAlpha.900' py='8px' fontWeight='thin'>
-                  Are you willing to travel? If so, how does this affect pricing?
-                </Text>
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </Heading>
-          <AccordionPanel pb={4}>
-            <Text fontWeight='bold' fontColor='blackAlpha.900' fontSize='24px'>Yes!</Text>
-            <Text fontSize='20px' color='blackAlpha.700'>
-              I will travel to the greater Nashville area for meetups. For sesions that require travel, 
-              overall price will be the total price of the session + traveling fee. Contact me if you
-              have any questions.
-            </Text>
-          </AccordionPanel>
-        </AccordionItem>
-
-        <AccordionItem>
-          <Heading as='h3'>
-            <AccordionButton  color='blackAlpha.900' 
-                              _expanded={{ bg: 'yellow.400', color: 'white' }} 
-                              fontSize='32px'>
-              <Box flex='1' textAlign='left' fontSize='24px'>
-                <Text fontSize='24px' noOfLines={2} color='blackAlpha.900' py='8px' fontWeight='thin'>
-                  Do I have to pay for my stems?
-                </Text>
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </Heading>
-          <AccordionPanel pb={4}>
-            <Text fontWeight='bold' fontColor='blackAlpha.900' fontSize='24px'>No!</Text>
-            <Text fontSize='20px' color='blackAlpha.700'>
-              Unlike other studios, I will not hold your stems. After each session,
-              I will begin compiling your stems so they can be sent to you within the same
-              week.
-            </Text>
-          </AccordionPanel>
-        </AccordionItem>
-        <AccordionItem>
-          <Heading as='h3'>
-            <AccordionButton  color='blackAlpha.900' 
-                              _expanded={{ bg: 'yellow.400', color: 'white' }} 
-                              fontSize='32px'>
-              <Box flex='1' textAlign='left' fontSize='24px'>
-                <Text fontSize='24px' noOfLines={2} color='blackAlpha.900' py='8px' fontWeight='thin'>
-                Do you offer package deals?
-                </Text>
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </Heading>
-          <AccordionPanel pb={4}>
-            <Text fontSize='24px' color='blackAlpha.900' fontWeight='bold'>Yes!</Text>
-            <Text fontSize='20px' color='blackAlpha.700'>
-             On top of individual services such as mixing, producing, and tracking, I also
-             offer packages. Checkout the pricing section to see which packages are available currently.
-             Don't see what you're looking for? Contact me about creating custom packages.
-            </Text>
-          </AccordionPanel>
-        </AccordionItem>
+        {
+          data.map( ({ question, answer }) => {
+            return (
+              <AccordionItem>
+                <Heading as='h3'>
+                  <AccordionButton  color='blackAlpha.900'
+                                    _expanded={{bg: 'yellow.400', color: 'white' }} 
+                                    fontSize='32px'>
+                    <Box flex='1' textAlign='left' fontSize='24px'>
+                      <Text fontSize='24px' noOfLines={2} color='blackAlpha.900' py='8px' fontWeight='thin'>
+                        {question}
+                      </Text>
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </Heading>
+                <AccordionPanel pb={4}>
+                  <Text fontSize='20px' color='blackAlpha.700' noOfLines={5}>
+                    {answer}
+                  </Text>
+                </AccordionPanel>
+              </AccordionItem>
+            );
+          })
+        }
       </Accordion>
     </Box>
   );
 }
+
+{/* <AccordionPanel pb={4}>
+  <Text fontSize='24px' color='blackAlpha.900' fontWeight='bold'>Yes!</Text>
+  <Text fontSize='20px' color='blackAlpha.700'>
+    {panels[3].question}
+  </Text>
+</AccordionPanel> */}
 
 export default MultiExpansionAccordion;
---
+++ src/data/accordion-items.data.js
@@ -0,0 +1,26 @@
+export const accordionItems = [
+  {
+    question: 'How long is the average session?',
+    answer: `Sessions range from 1 hour to 3 hours. For most clients, the typical session
+    is roughly 2 hours. If more time is needed, arrangements can be made 2 days before
+    the scheduled session. Contact me to discuss more details.`
+  },
+  {
+    question: 'Are you willing to travel? If so, how does this affect pricing?',
+    answer: `I will travel to the greater Nashville area for meetups. For sesions that require travel, 
+    overall price will be the total price of the session + traveling fee. Contact me if you
+    have any questions.`
+  },
+  {
+    question: 'Do I have to pay for my stems?',
+    answer: `Unlike other studios, I will not hold your stems. After each session,
+    I will begin compiling your stems so they can be sent to you within the same
+    week.`
+  },
+  {
+    question: 'Do you offer package deals?',
+    answer: `On top of individual services such as mixing, producing, and tracking, I also
+    offer packages. Checkout the pricing section to see which packages are available currently.
+    Don't see what you're looking for? Contact me about creating custom packages.`
+  },
+];
