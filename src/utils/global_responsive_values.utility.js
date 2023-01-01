8065ab2ae37a0f16727066df10bed956216d8471
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-12-31 22:54:37 -0600

    Tree-shake unused styles in global_responsive_values.utility.js

--- src/utils/global_responsive_values.utility.js
+++ src/utils/global_responsive_values.utility.js
@@ -3,7 +3,6 @@ const responsiveHorizontalPadding = ['16px', '32px', '64px', null, null];
 const responsiveVerticalPadding = ['64px', null, '128px', null, null];
 const responsiveIconSize = ['32px', '40px', '48px', '64px', null]
 
-
 export {
   responsiveHorizontalPadding,
   responsiveVerticalPadding,
