20c1128a947f58c993e21948bcdf04a96eb3032b
jdcodes-12 <johnnydicksoncode@gmail.com>
2023-01-01 00:16:31 -0600

    Refactor project structure
    Created list-items/ subdirectory in components/lists/ also refactored SongCarousel mimic project structure standard

---
+++ src/components/lists/list-items/icon-list-item/iconlistitem.component.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import {
+  responsiveListHeaderFontSize,
+  responsiveListBodyTextFontSize,
+  responsiveListBoxWidth,
+  responsiveListHeaderVerticalPadding,
+  responsiveListBodyVerticalPaddingBottom,
+} from './iconlistitem.responsive_values';
+
+import {
+  Flex,
+  Heading,
+  List,
+  ListItem,
+  ListIcon,
+} from '@chakra-ui/react';
+
+
+// TODO: Reponsive sizing
+const IconListItem = ({ icon, details={} }) => {
+  const {header, items, id, iconColor } = details;
+  
+  return (
+    <Flex direction='column'
+          align='center'
+          w='full'
+          >
+      <Heading as='h3'
+               fontSize={responsiveListHeaderFontSize}
+               color='whiteAlpha.900'
+               py={responsiveListHeaderVerticalPadding}
+               >
+        {header}
+      </Heading>
+      <List fontSize={responsiveListBodyTextFontSize}
+            color='whiteAlpha.700'
+            spacing='24px'
+            w={responsiveListBoxWidth}
+            pb={responsiveListBodyVerticalPaddingBottom}
+            >
+        {
+          items.map(
+            (text) => (
+              <ListItem key={id} 
+                        noOfLines={3} 
+                        overflow='hidden'
+                        textAlign='left'
+                        >
+                <ListIcon as={icon} color={iconColor} />
+                {text}
+              </ListItem>
+            )
+          )
+        }
+      </List>
+    </Flex>
+  );
+}
+
+export default IconListItem;
---
+++ src/components/lists/list-items/icon-list-item/iconlistitem.responsive_values.jsx
@@ -0,0 +1,16 @@
+// Typography & Sizing
+const responsiveListHeaderFontSize = ['40px', '40px' , '48px', '64px', null];
+const responsiveListBodyTextFontSize = ['18px', '32px' , '48px', '64px', null];
+const responsiveListBoxWidth = ['80%', '80%', 'full', null, null];
+
+// Margin & Padding
+const responsiveListHeaderVerticalPadding = ['24px', null, null, null, null];
+const responsiveListBodyVerticalPaddingBottom = ['16px', null, null, null, null];
+
+export {
+  responsiveListHeaderFontSize,
+  responsiveListBodyTextFontSize,
+  responsiveListBoxWidth,
+  responsiveListHeaderVerticalPadding,
+  responsiveListBodyVerticalPaddingBottom,
+}
