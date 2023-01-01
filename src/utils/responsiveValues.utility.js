df48ae2fb8cded5af9cb5841f0b109f169dabde4
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-12-31 20:40:28 -0600

    Create responsiveValues.utitilies.js
    The file holds all the responsive values that will be used throughout the website. The method of responsive values follows ChakraUI's array responsive syntax.

---
+++ src/utils/responsiveValues.utility.js
@@ -0,0 +1,25 @@
+// Sizing & Typography
+const responsiveFontSize = ['32px', '40px' , '48px', '64px', null];
+const responsiveNumberOfLines = [8, 8, 5, null, null];
+const responsiveIconSize = ['32px', '40px', '48px', '64px', null]
+
+// Padding & Margin
+const responsiveHorizontalPadding = ['16px', '32px', '64px', null, null];
+const responsiveVerticalPadding = ['64px', null, '128px', null, null]
+
+// Width & Height
+const responsiveWidth = ['80%', '50%','50%', '40%', null];
+// Layout (Flex)
+const responsiveFlexColumnAlign = ['']
+const responsiveFlexColumnJustify = ['']
+const responsiveFlexRowAlign = ['']
+
+export {
+  responsiveFontSize,
+  responsiveNumberOfLines,
+  responsiveHorizontalPadding,
+  responsiveVerticalPadding,
+  responsiveIconSize,
+  responsiveWidth,
+}
+ 
