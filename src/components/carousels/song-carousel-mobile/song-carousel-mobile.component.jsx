846f59bcc9eb8c7990d7760ab388de874e9e47cb
jdcodes-12 <johnnydicksoncode@gmail.com>
2023-01-20 16:44:04 -0600

    Refactor SongCarouselMobile
    Moved demo asset imports into demo-items.data.json so that any carousel component could map over demos to make SwiperSlides.

--- src/components/carousels/song-carousel-mobile/song-carousel-mobile.component.jsx
+++ src/components/carousels/song-carousel-mobile/song-carousel-mobile.component.jsx
@@ -3,10 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCoverflow } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
-import DemoOne from '../../../assets/demos/demo-1/demo-1.wav';
-import DemoTwo from '../../../assets/demos/demo-2/demo-2.wav';
-import DrillDemo from '../../../assets/demos/demo-3-drill/drill.wav';
-import SetMeFreeDemo from '../../../assets/demos/demo-4-set-me-free/demo-set-me-free.wav';
+import { demos } from '../../../data/items/demo-items.data';
 import SongCardMobile from '../../cards/song-card-mobile/song-card-mobile.component';
 
 import {
@@ -15,8 +12,9 @@ import {
 } from '@chakra-ui/react';
 
 const SongCarouselMobile = () => {
+
   return (
-    <Box py='64px' px='16px' w='full' border='1px'>
+    <Box py='64px' px='16px' w='full'>
       <Swiper
         effect={"coverflow"}
         centeredSlides={true}
@@ -31,18 +29,13 @@ const SongCarouselMobile = () => {
         className="mySwiper"
         onSlideChange={() => console.log("Changed")}
       >
-        <SwiperSlide>
-          <SongCardMobile src={DemoOne} title={'Demo One'}/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <SongCardMobile src={SetMeFreeDemo} title={'Set Me Free'}/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <SongCardMobile src={DemoTwo} title={'Demo Two'}/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <SongCardMobile src={DrillDemo} title={'Drill Demo'}/>
-        </SwiperSlide>
+       {
+        demos.map(demo => (
+          <SwiperSlide key={demo.id}>
+            <SongCardMobile src={demo.src} title={demo.title}/>
+          </SwiperSlide>
+        ))
+       }
       </Swiper>
     </Box>
   );
--- src/data/items/demo-items.data.js
+++ src/data/items/demo-items.data.js
@@ -1,5 +1,27 @@
-export const demoItems = [
+import DemoOne from '../../assets/demos/demo-1/demo-1.wav';
+import DemoTwo from '../../assets/demos/demo-2/demo-2.wav';
+import DrillDemo from '../../assets/demos/demo-3-drill/drill.wav';
+import SetMeFreeDemo from '../../assets/demos/demo-4-set-me-free/demo-set-me-free.wav';
+
+export const demos = [
   {
-    demoTitle: 'Set Me Free',
+    id: 1,
+    title: 'Set Me Free',
+    src: SetMeFreeDemo,
   },
+  {
+    id: 2,
+    title: 'Drill Demo',
+    src: DrillDemo,
+  },
+  {
+    id: 3,
+    title: 'Demo One',
+    src: DemoOne,
+  },  
+  {
+    id: 4,
+    title: 'Demo Two',
+    src: DemoTwo,
+  }
 ];
